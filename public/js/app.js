733175b9256af892a55eebf10a02473eae9bfee3
Mohammed Rafique <mohammedrafique23@gmail.com>
2021-05-04 23:04:42 +0530

    did code proper

--- public/js/app.js
+++ public/js/app.js
@@ -14,18 +14,18 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
     fetch('/weather?address=' + location).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            // console.log(data.error)
-            messageOne.textContent = data.error
-        } else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-            // console.log(data.location)
-            // console.log(data.forecast)
-        }
+        response.json().then((data) => {
+            if (data.error) {
+                // console.log(data.error)
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+                // console.log(data.location)
+                // console.log(data.forecast)
+            }
+        })
     })
-})
 
     // console.log(location)
 })
